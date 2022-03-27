e9f0fb711f6eb6d2d0b759726cd04ebb5d5e1c4f
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-03-27 16:01:27 -0300

    :sparkles: add todolists routes

--- lib/my_todo_list_web/router.ex
+++ lib/my_todo_list_web/router.ex
@@ -18,6 +18,7 @@ defmodule MyTodoListWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/todolists", TodoListController
   end
 
   # Other scopes may use custom stacks.
